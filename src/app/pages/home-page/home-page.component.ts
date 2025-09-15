d0270d0eb33945f93e60cc2c6349f77d58d43ce0
igonzalez <igonzalez@sre.gob.mx>
2025-09-14 22:35:23 -0600

    Agregando homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
+import { TaskListComponent } from '../../core/components/task-list/task-list.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MatGridListModule, CommonModule, TaskListComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
